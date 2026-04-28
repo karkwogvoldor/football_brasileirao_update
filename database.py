512aebf9acf27ac83d61077c511cdab1777cf430
Jerônimo <karkwogvoldor@gmail.com>
2026-04-27 21:33:17 -0300

    fix: usar DATABASE_URL do ambiente

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-DATABASE_URL = 'postgresql://postgres:postgres@localhost:5432/db_project'
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
